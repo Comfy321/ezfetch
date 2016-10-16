d9b9824748a1c439ca8b08daf8f0a9c1258e322b
LonelyPyxel <lonelypyxel@yandex.com>
2016-10-15 23:22:52 -0700

    added colors

--- ezfetch.sh
+++ ezfetch.sh
@@ -6,8 +6,8 @@ HOST="$(hostname)"
 KERNEL="$(uname -mrs)"
 OS="$(lsb_release -d)"
 SHELL="$(echo $SHELL)"
-    if [ -n "$DISPLAY" ]; then
-	id="$(xprop -root -notype | \awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}')"
+    if [ -n "$DISPLAY" ]; then #Credit to Mitya on IRC for this func.
+	id="$(xprop -root -notype | \awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}')" 
 	wm="$(xprop -id "$id" -notype -f _NET_WM_NAME 8t)"
 	wm="${wm/*_NET_WM_NAME = }"
 	wm="${wm/\"}"
@@ -32,4 +32,4 @@ Shell:          $SHELL
 | |_______________| |
 |___________________|
 "             
-
+echo -e " \033[1;37m██\033[0;30m██\033[0;34m██\033[1;34m██\033[0;32m██\033[1;32m██\033[0;36m██\033[1;36m██\033[0;31m██\033[1;31m██\033[0;35m██\033[1;35m██\033[0;33m██\033[1;33m██\033[1;30m██\033[0;37m██"
